1630e1826cae10f6342271408ebc448b07082945
levente.meszaros <levente.meszaros@gmail.com>
2010-03-16 13:12:40 +0000

    Added entity-relationship-diagram inspector.

--- source/entity-relationship-diagram.lisp
+++ source/entity-relationship-diagram.lisp
@@ -6,6 +6,9 @@
 
 (in-package :hu.dwim.meta-model)
 
+;;;;;;
+;;; entity-relationship-diagram
+
 (define-model-class entity-relationship-diagram (structure-diagram)
   ()
   (:documentation "An entity relationship diagram is a collection of entities and relationships between them."))
@@ -21,3 +24,21 @@
 
 (def (definer e) simple-entity-relationship-diagram (diagram-name model-element-names &rest args)
   `(def-simple-entity-relationship-diagram ,diagram-name ,model-element-names ,@args))
+
+;;;;;;
+;;; t/inspector
+
+(def layered-method make-alternatives ((component t/inspector) (class standard-class) (prototype entity-relationship-diagram) (value entity-relationship-diagram))
+  (list* (make-instance 'entity-relationship-diagram/documentation/inspector
+                        :component-value value
+                        :component-value-type (component-value-type-of component))
+         (call-next-method)))
+
+;;;;;;
+;;; entity-relationship-diagram/documentation/inspector
+
+(def (component e) entity-relationship-diagram/documentation/inspector (t/documentation/inspector)
+  ())
+
+(def method make-documentation ((component entity-relationship-diagram/documentation/inspector) (class standard-class) (prototype entity-relationship-diagram) (value entity-relationship-diagram))
+  (documentation-of value))
